6600204987b2673754a2d2096e77a1b3780a7554
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-09-28 20:42:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    
+		let firstName = document.getElementById("firstname").value.trim();
+		let lastName = document.getElementById("lastname").value.trim();
+
+		if( firstName !== "" && lastName !== "")
+		{
+			alert("First name:" +  firstName + "\nLast name:" + lastName);
+		}
+		else{
+			alert("Enter your first and last name.");
+		}
 
 }
